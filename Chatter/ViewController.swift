ac0d137e39213b1046bb45d214c8a458a613613b
Пётр Тесленко <peter@teslenko.net>
2019-01-28 16:19:43 +0300

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Peter Teslenko on 28/01/2019.
+//  Copyright © 2019 Petr Teslenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
